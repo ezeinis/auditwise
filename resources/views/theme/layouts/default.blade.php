fad17a8ea7c6d529eaa251e34a45c0bfac93e719
efthimis zeinis <e.zeinis@darkpony.com>
2016-07-28 12:11:12 +0300

    laravel fresh install 5.2.41, bootstrap 3.3.7

---
+++ resources/views/theme/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Auditwise</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap-3.3.7.min.css">
+    @yield('header')
+</head>
+<body>
+
+    @yield('nav_header')
+    @yield('content')
+    @yield('footer')
+
+    <script src="/js/bootstrap-3.3.7.min.js"></script>
+    @yield('js')
+</body>
+</html>
